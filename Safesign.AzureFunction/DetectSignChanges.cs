d28608e6d8b29df205dca19ba8001c9248ca59fb
unknown <g.vendeloe@hotmail.com>
2023-09-04 12:50:19 +0200

    Azure development, gitignore changes

--- Safesign.AzureFunction/DetectSignChanges.cs
+++ Safesign.AzureFunction/DetectSignChanges.cs
@@ -2,6 +2,7 @@ using Safesign.Core;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 using Microsoft.Azure.Functions.Worker.Http;
+using System.Net;
 
 namespace Safesign.AzureFunction
 {
@@ -11,16 +12,27 @@ namespace Safesign.AzureFunction
         private readonly HttpClient _httpClient;
         private readonly int angleOffSet = 5;
     
-    
-    // [Function("negotiate")]
-    // public static HttpResponseData Negotiate([HttpTrigger(AuthorizationLevel.Anonymous)] HttpRequestData req,
-    // [SignalRConnectionInfoInput(HubName = "serverless")] string connectionInfo)
-    //     {
-    //         var response = req.CreateResponse(HttpStatusCode.OK);
-    //         response.Headers.Add("Content-Type", "application/json");
-    //         response.WriteString(connectionInfo);
-    //         return response;
-    //     }
+
+        [Function("negotiate")]
+        public static HttpResponseData Negotiate([HttpTrigger(AuthorizationLevel.Anonymous)] HttpRequestData req,
+        [SignalRConnectionInfoInput(HubName = "serverless")] string connectionInfo)
+        {
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "application/json");
+            response.WriteString(connectionInfo);
+            return response;
+        }
+        
+        [Function("index")]
+        public static HttpResponseData GetHomePage([HttpTrigger(AuthorizationLevel.Anonymous)] HttpRequestData req)
+        {
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.WriteString(File.ReadAllText("content/index.html"));
+            response.Headers.Add("Content-Type", "text/html");
+            return response;
+        }
+
+
 
         public DetectSignChanges(ILoggerFactory loggerFactory, IHttpClientFactory httpClientFactory) 
         {
@@ -65,7 +77,7 @@ namespace Safesign.AzureFunction
 
         [Function(nameof(DetectAngleChange2))]
         [SignalROutput(HubName = "serverless")]
-        public async Task<SignalRMessageAction> DetectAngleChange2(
+        public SignalRMessageAction DetectAngleChange2(
             //[HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequestData requestData,
             [CosmosDBTrigger(
             databaseName: "Safesign",
@@ -88,14 +100,13 @@ namespace Safesign.AzureFunction
                     if(i.CurrAngle < i.OgAngle - angleOffSet || i.CurrAngle > i.OgAngle + angleOffSet) 
                     {
                         _logger.LogInformation($"Sign {i.Id} in project {i.PlanId} is angled incorrectly");
-                        await Task.Run(() => {
-                            return new SignalRMessageAction("SignAngleIssue", new object[] {i.Id,i.PlanId,i.CurrAngle});
-                        });
-                    } 
+                       // await Task.Run(() => {
+                        return new SignalRMessageAction("SignAngleIssue", new object[] {i});
+                       // });
+                    }
                 }
             }
             return null;
         }
-
     } // Class end
 } // Namespace end
