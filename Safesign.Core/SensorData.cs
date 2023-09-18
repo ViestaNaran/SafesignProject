1b608e7d2c63e352a6a01b9933bc5bb6d7c85db2
unknown <g.vendeloe@hotmail.com>
2023-09-18 15:55:36 +0200

    more cleanup. Removed MainResponsible class

--- Safesign.Core/SensorData.cs
+++ Safesign.Core/SensorData.cs
@@ -1,5 +1,7 @@
 namespace Safesign.Core {
 
+    // Variables do not follow standard naming conventions as to not interfere with the format from the sensor. 
+    // Will fix in the future
     public class SensorData
     {
         public string msg { get; set; }
